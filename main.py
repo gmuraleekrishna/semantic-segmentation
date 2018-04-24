8c5e259139906843cc6002bc0a152557f20d77d2
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-04-24 20:43:01 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
