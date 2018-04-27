e7435f384f3908a754e731120c820c19e6d53ead
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-04-27 18:36:57 +1000

    Logistic reggresion

--- main.py
+++ main.py
@@ -4,3 +4,43 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+import numpy as np
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+    
+test_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory('training_set',
+    target_size = (64, 64),
+    batch_size = 32,
+    class_mode = 'binary')
+test_set = test_datagen.flow_from_directory('test_set',
+    target_size = (64, 64),
+    batch_size = 32,
+    class_mode = 'binary')
+classifier.fit_generator(training_set,
+    steps_per_epoch = 8000,
+    epochs = 25,
+    validation_data = test_set,
+    validation_steps = 2000)
+
+
+test_image = image.load_img('cat.4989.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
